1a4a6eadb66336af24354a9a5d808adae30d2717
ForNeVeR <neverthness@gmail.com>
2011-10-22 17:45:14 +0700

    Calculation: - renamed to Tantalum.Core; - small fixes. Tantalum.Input: added stub for module based on FParsec.

---
+++ Tantalum/Core.fs
@@ -0,0 +1,29 @@
+﻿/// Tantalum core module.
+module Tantalum.Core
+    open System
+
+    let convert (symbol : string) =
+         Double.Parse(symbol)
+
+    type Constant =
+        | Symbol of string
+        | Double of double
+
+    type BinaryOperation =
+        | Addition
+        | Substraction
+    
+    type Node = 
+        | Operation of BinaryOperation * Node * Node
+        | Constant of Constant
+
+    let apply op =
+        match op with
+        | Addition     -> (+)
+        | Substraction -> (-)
+
+    let rec calculate node : double =
+        match node with
+        | Operation (operation, node1, node2) -> apply operation (calculate node1) (calculate node2)
+        | Constant (Symbol symbol)            -> convert symbol
+        | Constant (Double double)            -> double
---
+++ Tantalum/Input.fs
@@ -0,0 +1,17 @@
+﻿/// Tantalum command line input module.
+module Tantalum.Input
+    open System
+    open FParsec
+    open Tantalum.Core
+
+    let parse message =
+        Operation (Addition, Constant (Double 2.0), Constant (Symbol "2"))
+
+    let rec repl =
+        Console.Write "> "
+        let input = Console.ReadLine ()
+        let operation = parse input
+        let output = calculate operation
+        Console.WriteLine output
+
+    repl
