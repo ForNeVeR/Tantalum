82d8ecddcfc641f991ba3e48f998f9969ea2e2bc
ForNeVeR <neverthness@gmail.com>
2011-11-01 21:51:13 +0700

    Tantalum.Executor: stub class improved.

--- Tantalum/Executor.fs
+++ Tantalum/Executor.fs
@@ -1,3 +1,9 @@
 ﻿namespace Tantalum
     type Executor =
-        
+        member executor.AddUnaryFunction (func : Function) (applyFunctor : double -> double) = ()
+        member executor.AddBinaryFunction (func : Function) (applyFunctor : double * double -> double) = ()
+
+        member executor.AddPattern (pattern : Pattern) = ()
+
+        member executor.CalculateSymbolic (expression: ExecutionTree) = ()
+        member executor.CalculateBinary (expression: ExecutionTree) = ()
