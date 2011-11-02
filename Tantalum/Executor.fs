3b65a7281aebafde0bf041144a92d459ec07baad
ForNeVeR <neverthness@gmail.com>
2011-11-02 21:17:20 +0700

    Tantalum.Executor: function application implemented.

--- Tantalum/Executor.fs
+++ Tantalum/Executor.fs
@@ -1,9 +1,31 @@
 ﻿namespace Tantalum
+    open System.Collections.Generic
     open System.Linq
 
+    type private Functor = double list -> double
+
     type Executor () =
-        member executor.AddUnaryFunction (func : Function) (applyFunctor : double -> double) = ()
-        member executor.AddBinaryFunction (func : Function) (applyFunctor : double * double -> double) = ()
+        let functions = new Dictionary<Function, Functor> ()
+
+        member executor.AddUnaryFunction (func : Function) (applyFunctor : double -> double) =
+            if func.Arity = 1
+            then
+                functions.[func] <-
+                    fun args ->
+                        match args with
+                        | [arg] -> applyFunctor arg
+                        | _     -> failwith "Wrong number of arguments for unary function."
+            else failwith "Wrong unary function definition."
+
+        member executor.AddBinaryFunction (func : Function) (applyFunctor : double * double -> double) =
+            if func.Arity = 2
+            then
+                functions.[func] <-
+                    fun args ->
+                        match args with
+                        | [arg1; arg2] -> applyFunctor (arg1, arg2)
+                        | _            -> failwith "Wrong number of arguments for binary function."
+            else failwith "Wrong binary function definition."
 
         member executor.AddSimplificationPattern (pattern : Pattern) = ()
         member executor.AddNormalizationPattern (pattern : Pattern) = ()
@@ -16,5 +38,9 @@
             | Constant (Double d)               -> d
             | Constant (Symbolic s)             -> s.ToBinary ()
             | Function (func, args)
-                when func.Arity = args.Count () -> failwith "Function application still not implemented."
+                when func.Arity = args.Count () ->
+                    let apply = functions.[func]
+                    Seq.map executor.CalculateBinary args
+                    |> Seq.toList
+                    |> apply 
             | Function _                        -> failwith "Wrong number of arguments."
