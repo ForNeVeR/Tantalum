358d2b922fbad4721e45f5c0a17ab5dc675c74b1
ForNeVeR <neverthness@gmail.com>
2013-03-16 17:47:46 +0700

    Implement executor test.

---
+++ Tests/ExecutorTest.fs
@@ -0,0 +1,22 @@
+﻿namespace Tantalum.Test
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open Tantalum
+
+[<TestClass>]
+type ExecutorTest() =
+    static let parser = Voice.createParser()
+    static let executor = Executor.executor()
+    static do StandardOperations.Register executor
+
+    static let expression text =
+        Voice.Parse parser text
+
+    [<TestMethod>]
+    member this.SimpleExpression() =
+        let x = expression "2 + 2 * 2"
+        let simplified = executor.CalculateSymbolic x
+        let result = executor.CalculateBinary x
+        let expected = expression "6"
+        Assert.AreEqual(expected, simplified)
+        Assert.AreEqual(6, result)
--- Voice/Program.fs
+++ Voice/Program.fs
@@ -6,52 +6,55 @@ open FParsec
 open Tantalum
 open Tantalum.Executor
 
-let private expressionParser = new OperatorPrecedenceParser<Expression, unit, unit> ()
-let private expression = expressionParser.ExpressionParser
-let private number =
-    regex @"[\d]+(\.[\d]+)?([eE][+-]?[\d]+)?"
-    |>> fun s -> Constant <| Symbol.Create s
+let createParser() =
+    let expressionParser = new OperatorPrecedenceParser<Expression, unit, unit> ()
+    let expression = expressionParser.ExpressionParser
+    let number =
+        regex @"[\d]+(\.[\d]+)?([eE][+-]?[\d]+)?"
+        |>> fun s -> Constant <| Symbol.Create s
 
-let private tokenParser =
-    (spaces >>. number .>> spaces)
-    <|> between (pstring "(") (pstring ")") expression
+    let tokenParser =
+        (spaces >>. number .>> spaces)
+        <|> between (pstring "(") (pstring ")") expression
 
-expressionParser.TermParser <- tokenParser
+    expressionParser.TermParser <- tokenParser
 
-/// Registers prefix unary operator for use in input stream.
-let RegisterUnaryOperator (symbol : string) (priority : int) : unit =
-    expressionParser.AddOperator
-    <| PrefixOperator (symbol, spaces, priority, false,
-                        fun a -> Function ({Id = symbol; Arity = 1}, [a]))
+    /// Registers prefix unary operator for use in input stream.
+    let RegisterUnaryOperator (symbol : string) (priority : int) : unit =
+        expressionParser.AddOperator
+        <| PrefixOperator (symbol, spaces, priority, false,
+                            fun a -> Function ({Id = symbol; Arity = 1}, [a]))
 
-/// Registers binary operator for use in input stream.
-let RegisterBinaryOperator (symbol : string) (priority : int) (associativity : Associativity) =
-    expressionParser.AddOperator
-    <| InfixOperator (symbol, spaces, priority, associativity,
-                        fun a b -> Function ({Id = symbol; Arity = 2}, [a; b]))
+    /// Registers binary operator for use in input stream.
+    let RegisterBinaryOperator (symbol : string) (priority : int) (associativity : Associativity) =
+        expressionParser.AddOperator
+        <| InfixOperator (symbol, spaces, priority, associativity,
+                            fun a b -> Function ({Id = symbol; Arity = 2}, [a; b]))
 
-let RegisterUnaryFunction (name : string) =
-    expressionParser.AddOperator
-    <| PrefixOperator (name, spaces, 3, false,
-                        fun expr -> Function ({Id = name; Arity = 1}, [expr]))
+    let RegisterUnaryFunction (name : string) =
+        expressionParser.AddOperator
+        <| PrefixOperator (name, spaces, 3, false,
+                            fun expr -> Function ({Id = name; Arity = 1}, [expr]))
 
-RegisterUnaryOperator "+" 1
-RegisterUnaryOperator "-" 1
+    RegisterUnaryOperator "+" 1
+    RegisterUnaryOperator "-" 1
 
-RegisterBinaryOperator "-" 1 Associativity.Left
-RegisterBinaryOperator "+" 1 Associativity.Left
-RegisterBinaryOperator "*" 2 Associativity.Left
-RegisterBinaryOperator "/" 2 Associativity.Left
-RegisterBinaryOperator "^" 3 Associativity.Right
+    RegisterBinaryOperator "-" 1 Associativity.Left
+    RegisterBinaryOperator "+" 1 Associativity.Left
+    RegisterBinaryOperator "*" 2 Associativity.Left
+    RegisterBinaryOperator "/" 2 Associativity.Left
+    RegisterBinaryOperator "^" 3 Associativity.Right
 
-RegisterUnaryFunction "sin"
-RegisterUnaryFunction "cos"
-RegisterUnaryFunction "tg"
-RegisterUnaryFunction "ctg"
+    RegisterUnaryFunction "sin"
+    RegisterUnaryFunction "cos"
+    RegisterUnaryFunction "tg"
+    RegisterUnaryFunction "ctg"
+
+    expression
 
 /// Parses input string and returns corresponding Expression.
-let Parse (input : string) : Expression =
-    match run expression input with
+let Parse parser input : Expression =
+    match run parser input with
     | Success (result, _, _) -> result
     | Failure (msg, err, _)  -> failwith msg
 
@@ -60,12 +63,13 @@ let private executor = executor ()
 StandardOperations.Register executor
 
 let private repl =
+    let parser = createParser()
     while true do
         Console.Write "> "
         let input = Console.ReadLine ()
         let result = 
             try
-                let inputExpr = Parse input
+                let inputExpr = Parse parser input
                 let optimizedExpr = executor.CalculateSymbolic inputExpr
                 let output = executor.CalculateBinary optimizedExpr
                 sprintf "%s = %s = %sb" (inputExpr.ToString ()) (optimizedExpr.ToString ()) (output.ToString ())
